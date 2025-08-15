b880a3b2a484ddac72850fc7183b88387f292cdb
archbee12 <debbieokon32@gmail.com>
2025-08-15 17:28:22 +0100

    fixing path

--- js/introRecipes.js
+++ js/introRecipes.js
@@ -2,22 +2,22 @@
 export const introRecipes = [
   {
     title: "Spaghetti Carbonara",
-    image: "/images/spaghetti.webp",
+    image: "images/spaghetti.webp",
     description: 'Classic Italian pasta with creamy sauce and pancetta.'
   },
   {
     "title": "Potato Frittata Muffin",
     "description": "A savory, bite-sized twist on the classic frittata packed with shredded potatoes, eggs, cheese, and herbs. ",
-    "image": "/images/fitata.webp"
+    "image": "images/fitata.webp"
   },
   {
     "title": "Burger",
     "description": "A juicy grilled patty tucked between soft buns, layered with fresh lettuce, tomato, cheese, and your favorite condiments.",
-    "image": "/images/burger.webp"
+    "image": "images/burger.webp"
   },
   {
     title: 'Berry Smoothie Bowl',
-    image: "../images/berry.webp",
+    image: "images/berry.webp",
     description: 'Refreshing and nutritious bowl topped with fresh fruits.'
   }
 ];
